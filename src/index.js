7f0225197b11d2f7ff5bd5d9394ef54fa4d45be1
Shaurya Dahiya <shauryadahiya@gmail.com>
2023-02-07 19:12:29 +0530

    mainPage added

--- src/index.js
+++ src/index.js
@@ -10,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    {/* <Mainpage /> */}
-    
-    {/* <App /> */}
+    <Mainpage />
   </React.StrictMode>
 );
 
