28e14b1a75773b75ad24facfd99892f923e057b7
Shaurya Dahiya <shauryadahiya@gmail.com>
2023-02-07 19:10:31 +0530

    Header Added

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import Mainpage from './components/Mainpage'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Header />
-    <Mainpage /> */}
+    <Header />
+    {/* <Mainpage /> */}
     
-    <App />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
