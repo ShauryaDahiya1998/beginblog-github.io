921d8af1a560f630e1f567e40a0ce358e91582d5
Shaurya Dahiya <shauryadahiya@gmail.com>
2023-05-26 17:53:28 +0530

    removed margin

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,10 +3,10 @@ import Mainpage from "../components/Mainpage.js";
 
 const HomePage = () => {
     return(
-        <>
+        <div >
             <Header />
             <Mainpage />
-        </>
+        </div>
     )
 }
 
